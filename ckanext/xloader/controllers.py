94144de48618b9942268c742497c3b44aabc54fb
Ian Ward <ian@excess.org>
2021-12-06 16:34:32 -0500

    actually reupload when user clicks upload

--- ckanext/xloader/controllers.py
+++ ckanext/xloader/controllers.py
@@ -10,8 +10,11 @@ class ResourceDataController(p.toolkit.BaseController):
         if p.toolkit.request.method == 'POST':
             try:
                 p.toolkit.c.pkg_dict = \
-                    p.toolkit.get_action('xloader_submit')(
-                        None, {'resource_id': resource_id}
+                    p.toolkit.get_action("xloader_submit")(
+                        None, {
+                            "resource_id": resource_id,
+                            "ignore_hash": True,  # user clicked the reload button
+                        }
                     )
             except p.toolkit.ValidationError:
                 pass
